451bd5e9abe5156bfa106805eb60215466fd0ac3
H3adlock <sourav.ghosh700@hotmail.com>
2019-07-01 09:54:05 +0530

    initiating report module

--- report/models.py
+++ report/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 from django.utils import timezone
+
+
+class report(models.Model):
+    firstoccurrence = models.DateTimeField()
+    node = models.CharField(max - legth=50)
+    severity = models.CharField(max - legth=20)
+    alarm = models.TextField()
