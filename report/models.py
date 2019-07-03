a6d51797ed364ff0baf56bc6058c99c8214b19e2
H3adlock <sourav.ghosh700@hotmail.com>
2019-07-03 10:13:30 +0530

    vscode setup done

--- report/models.py
+++ report/models.py
@@ -4,6 +4,6 @@ from django.utils import timezone
 
 class report(models.Model):
     firstoccurrence = models.DateTimeField()
-    node = models.CharField(max - legth=50)
-    severity = models.CharField(max - legth=20)
+    node = models.CharField(max_length=50)
+    severity = models.CharField(max_length=20)
     alarm = models.TextField()
